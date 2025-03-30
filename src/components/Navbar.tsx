2500fb53a7c88a3ceb5e1d14770afa21fa9ace01
AlbertKistowski <118359839+Bercik4200@users.noreply.github.com>
2025-03-30 21:24:58 +0200

    Menu burgerowe

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,32 @@
+'use client';
+
+import { useState } from 'react';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(prev => !prev);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav className={styles.nav}>
       <div className={styles.logo}>Voyage Venture</div>
-      <ul className={styles.menu}>
-        <li><a href="#">Start</a></li>
-        <li><a href="#">Wyprawy</a></li>
-        <li><a href="#">O nas</a></li>
-        <li><a href="#">Kontakt</a></li>
+
+      <div className={styles.burger} onClick={toggleMenu}>
+        {menuOpen ? '✕' : '☰'}
+      </div>
+
+      <ul className={`${styles.menu} ${menuOpen ? styles.open : ''}`}>
+        <li><a href="#" onClick={closeMenu}>Start</a></li>
+        <li><a href="#" onClick={closeMenu}>Wyprawy</a></li>
+        <li><a href="#" onClick={closeMenu}>O nas</a></li>
+        <li><a href="#" onClick={closeMenu}>Kontakt</a></li>
       </ul>
     </nav>
   );
